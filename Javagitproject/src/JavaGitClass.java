5336296f3cf07cf6379d1af14a041ae30d7535a0
minakshid6 <104327664+minakshid6@users.noreply.github.com>
2022-04-24 23:30:01 -0500

    5th commit

--- Javagitproject/src/JavaGitClass.java
+++ Javagitproject/src/JavaGitClass.java
@@ -5,7 +5,7 @@ public static void main(String[] args) {
 	System.out.println("this is java git demo ");
 	System.out.println("hi this is krish");
 	System.out.println("hi this is Jinal");
-	
+	System.out.println("hi this is Minakshi");
 	
 }
 }
