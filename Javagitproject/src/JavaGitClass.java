a358cf9ffecea2fbd73ca18d7eab3b86e21632f8
minakshid6 <104327664+minakshid6@users.noreply.github.com>
2022-04-24 23:27:40 -0500

    third commit

--- Javagitproject/src/JavaGitClass.java
+++ Javagitproject/src/JavaGitClass.java
@@ -3,5 +3,9 @@ public class JavaGitClass {
 public static void main(String[] args) {
 	System.out.println("Hello World");
 	System.out.println("this is java git demo ");
+	System.out.println("hi this is krish");
+	
+	
+	
 }
 }
